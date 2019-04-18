6f5ce76a76f6ab56fa47d2a6f3e629d37c6ad85c
Martin Sandström <martin@marteinn.se>
2019-04-18 15:27:24 +0200

    Rename implemnetation function to *_impl (no prefix)

--- wagtailtextanalysis/providers/azure_text_analytics.py
+++ wagtailtextanalysis/providers/azure_text_analytics.py
@@ -3,7 +3,27 @@ from django.utils.translation import get_language
 import requests
 
 
-def _get_key_phrases_impl(json_data):
+def get_key_phrases(text, identifier):
+    lang_and_country_code = get_language()
+    lang_code = lang_and_country_code.split("-")[0]
+
+    json_data = {
+        "documents": [
+            {"id": identifier, "language": lang_code, "text": text}
+        ]
+    }
+
+    json_response = get_key_phrases_impl(json_data)
+
+    documents = json_response["documents"]
+
+    if len(documents) == 0:
+        return []
+
+    return json_response["documents"][0]["keyPhrases"]
+
+
+def get_key_phrases_impl(json_data):
     headers = {
         'Content-Type': 'application/json',
         'Ocp-Apim-Subscription-Key': settings.AZURE_TEXT_ANALYTICS_API_KEY,
@@ -18,26 +38,6 @@ def _get_key_phrases_impl(json_data):
 
     response.raise_for_status()
     return response.json()
-
-
-def get_key_phrases(text, identifier):
-    lang_and_country_code = get_language()
-    lang_code = lang_and_country_code.split("-")[0]
-
-    json_data = {
-        "documents": [
-            {"id": identifier, "language": lang_code, "text": text}
-        ]
-    }
-
-    json_response = _get_key_phrases_impl(json_data)
-
-    documents = json_response["documents"]
-
-    if len(documents) == 0:
-        return []
-
-    return json_response["documents"][0]["keyPhrases"]
 
 
 def get_api_domain(region):
