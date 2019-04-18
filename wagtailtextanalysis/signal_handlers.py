3a44e240b4a6aa339e62bc3384bb1cda1c08e697
Martin Sandström <martin@marteinn.se>
2019-04-18 15:24:57 +0200

    Add uid and only use pre_save hook

--- wagtailtextanalysis/signal_handlers.py
+++ wagtailtextanalysis/signal_handlers.py
@@ -1,12 +1,16 @@
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import pre_save
 
 from .text_analysis import get_text_analysis_models, analyse
 
 
-def post_save_signal_handler(instance, **kwargs):
+def pre_save_signal_handler(instance, *args, **kwargs):
     analyse(instance)
 
 
 def register_signal_handlers():
     for model in get_text_analysis_models():
-        pre_save.connect(post_save_signal_handler, sender=model)
+        pre_save.connect(
+            pre_save_signal_handler,
+            sender=model,
+            dispatch_uid=f"wagtailtextanalysis.pre_save.{model.__module__}",
+        )
